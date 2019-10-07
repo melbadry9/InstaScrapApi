b4b47fb43300574d7fda5adf40a91e840147534a
Mohamed Elbadry <melbadry9@github.com>
2019-10-07 12:53:05 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name= "InstaScrapApi",
-    version= "1.0.0",
+    version= "1.1.0",
     author= "Mohamed Elbadry",
     author_email= "mohamed.sa.elbadry@gmail.com",
     packages= find_packages(),
